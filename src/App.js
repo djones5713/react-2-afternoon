41aa35c51a5e1ba4542bbcca1750a32bef38e7dd
Destiny Jones <destinyjones@Destinys-MacBook.local>
2019-05-06 16:23:23 -0700

    finished project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import TextBox from './component/TextBox';
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TextBox />
+      </div>
+    );
+  }
+}
+
+export default App;
